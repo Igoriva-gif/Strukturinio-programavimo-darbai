791499c2395d5b62a4aab03d3b76471b5c954ff4
Igor Ivanov <s057739@ad.viko.lt>
2024-09-12 11:46:00 +0300

    parasytas kodo fragmentas

--- Pirmas darbas/main.cpp
+++ Pirmas darbas/main.cpp
@@ -1,7 +1,63 @@
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, World!" << std::endl;
+int main() {
+    string vardas, pavarde, grupe, spv;
+    int amzius, kursas;
+
+        vardas = "Igor";
+        pavarde = "Ivanov";
+        grupe = "PI";
+        spv = "Programu sistemos";
+
+        amzius = 22;
+        kursas = 1;
+
+        cout<<"\n"<<endl;
+        cout<<"--------------1UZD--------------"<<endl;
+        cout<<"\n"<<endl;
+
+        cout<<"Studento vardas: "<< vardas << endl;
+        cout<<"Studento pavarde: "<< pavarde << endl;
+        cout<<"Studento amzius: "<< amzius << endl;
+        cout<<"Studento grupe: "<< grupe << endl;
+        cout<<"Studento kursas: " << kursas << endl;
+        cout<<"Studento studiju programos pavadinimas: " << spv << endl;
+
+        cout<<"\n"<<endl;
+        cout<<"--------------2UZD--------------"<<endl;
+        cout<<"\n"<<endl;
+
+    string name, sav, arena;
+    int metai, vt;
+
+    name = "Vilniaus Rytas";
+    sav = "Vilniaus miesto savivaldybes administracija";
+    arena = "Jeep arena, Vilnius";
+    metai = 1997;
+    vt = 2500;
+
+    cout<<"Krepsinio klubo pavadinimas: "<< name <<endl;
+    cout<<"Ikurimo metai: "<< metai <<endl;
+    cout<<"Klubo savininkas: "<< sav <<endl;
+    cout<<"Arena: "<< arena <<endl;
+    cout<<"Vietu skaicius: "<< vt <<endl;
+
+    cout<<"\n"<<endl;
+    cout<<"--------------3UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    string marke, modelis, spalva;
+    int metai;
+    float litr;
+
+    marke = BMW;
+    modelis = E36 Coupe;
+    spalva = Juodos;
+    metai = 1993;
+    litr = 3,0d;
+
+    cout<<"Automobilis " << marke <<endl;
     return 0;
 }
+
---
+++ Pirmas darbas/main2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name, sav, arena;
+    int metai, vt;
+
+    name = "Vilniaus Rytas";
+    sav = "Vilniaus miesto savivaldybes administracija";
+    arena = "Jeep arena, Vilnius";
+    metai = 1997;
+    vt = 2500;
+
+    cout<<"Krepsinio klubo pavadinimas: "<< name <<endl;
+    cout<<"Ikurimo metai: "<< name <<endl;
+    cout<<"Klubo savininkas: "<< name <<endl;
+    cout<<"Arena: "<< name <<endl;
+    cout<<"Vietu skaicius: "<< name <<endl;
+
+    return 0;
+}
