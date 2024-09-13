5acf14c0c4cbe6665445309722dd5d8135a894ce
Igor Ivanov <s057739@ad.viko.lt>
2024-09-13 09:57:21 +0300

    a valid message

--- Pirmas darbas/main.cpp
+++ Pirmas darbas/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ranges>
 using namespace std;
 
 int main() {
@@ -13,29 +14,29 @@ int main() {
         amzius = 22;
         kursas = 1;
 
-        cout<<"\n"<<endl;
-        cout<<"--------------1UZD--------------"<<endl;
-        cout<<"\n"<<endl;
+    cout<<"\n"<<endl;
+    cout<<"--------------1UZD--------------"<<endl;
+    cout<<"\n"<<endl;
 
-        cout<<"Studento vardas: "<< vardas << endl;
-        cout<<"Studento pavarde: "<< pavarde << endl;
-        cout<<"Studento amzius: "<< amzius << endl;
-        cout<<"Studento grupe: "<< grupe << endl;
-        cout<<"Studento kursas: " << kursas << endl;
-        cout<<"Studento studiju programos pavadinimas: " << spv << endl;
+    cout<<"Studento vardas: "<< vardas << endl;
+    cout<<"Studento pavarde: "<< pavarde << endl;
+    cout<<"Studento amzius: "<< amzius << endl;
+    cout<<"Studento grupe: "<< grupe << endl;
+    cout<<"Studento kursas: " << kursas << endl;
+    cout<<"Studento studiju programos pavadinimas: " << spv << endl;
 
-        cout<<"\n"<<endl;
-        cout<<"--------------2UZD--------------"<<endl;
-        cout<<"\n"<<endl;
+    cout<<"\n"<<endl;
+    cout<<"--------------2UZD--------------"<<endl;
+    cout<<"\n"<<endl;
 
     string name, sav, arena;
     int metai, vt;
 
-    name = "Vilniaus Rytas";
-    sav = "Vilniaus miesto savivaldybes administracija";
-    arena = "Jeep arena, Vilnius";
-    metai = 1997;
-    vt = 2500;
+        name = "Vilniaus Rytas";
+        sav = "Vilniaus miesto savivaldybes administracija";
+        arena = "Jeep arena, Vilnius";
+        metai = 1997;
+        vt = 2500;
 
     cout<<"Krepsinio klubo pavadinimas: "<< name <<endl;
     cout<<"Ikurimo metai: "<< metai <<endl;
@@ -48,16 +49,57 @@ int main() {
     cout<<"\n"<<endl;
 
     string marke, modelis, spalva;
-    int metai;
+    int metai1;
     float litr;
 
-    marke = BMW;
-    modelis = E36 Coupe;
-    spalva = Juodos;
-    metai = 1993;
-    litr = 3,0d;
+        marke = "BMW";
+        modelis = "E36 Coupe";
+        spalva = "juodos";
+        metai1 = 1993;
+        litr = 1.8;
 
-    cout<<"Automobilis " << marke <<endl;
+    cout<<"Automobilis "<<marke<<modelis<<" yra pagamintas "<<metai1<<" metais."<<" Jo motoras "<<litr<<" litrazo. "<<"Automobilis yra "<<spalva<<" spalvos."<<endl;
+
+    cout<<"\n"<<endl;
+    cout<<"--------------4UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    int a, b;
+    float c;
+
+        a = 13;
+        b = 5;
+        c = 17.5;
+
+    cout << a + b - c << endl;
+    cout << 15/2 + c << endl;
+    cout << a/static_cast<double>(b) + 2*c << endl;
+    cout << 14%3 + 6.3 + b/a << endl;
+    cout << static_cast<int>(c)%5 + a - b << endl;
+    cout << 13.5/2 + 4.0 * 3.5 + 18 << endl;
+
+    cout<<"\n"<<endl;
+    cout<<"--------------5UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    int q, w, e, r, t;
+
+        cout << "Iveskite Jusu skaicius:" << endl;
+        cin >> q;
+        cin >> w;
+        cin >> e;
+        cin >> r;
+        cin >> t;
+        cout << "Ivestu skaiciu vidurkis yra: " << ( q + w + e + r + t )/5 << endl;
+
+    cout<<"\n"<<endl;
+    cout<<"--------------6UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    int d;
+
+
+
     return 0;
 }
 
