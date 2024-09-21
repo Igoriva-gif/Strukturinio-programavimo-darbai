96dd0823e19316c6e2bedf29bf9db842ab778902
Igor Ivanov <s057739@ad.viko.lt>
2024-09-21 13:05:47 +0300

    Pabaigiau uzduotys su ciklais

---
+++ SalygosIrJungiklioOperatoriai/main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    /*
+            cout<<"\n"<<endl;
+            cout<<"--------------1 ir 2 UZD--------------"<<endl;
+            cout<<"\n"<<endl;
+    int a, b, c, d, e;
+        cout<<"Iveskite studeno pazymius"<<endl;
+        cin>>a;
+        cin>>b;
+        cin>>c;
+        cin>>d;
+        cin>>e;
+    int sum = ((a+b+c+d+e)/5);
+    if (sum>=5) {
+        cout<<"Studento pazymiu vidurkis yra teigiamas ir lygus: "<<sum<<endl;
+    }
+        else {
+            cout<<"Studento pazymiu vidurkis yra neeigiamas ir lygus:"<<sum<<endl;
+    }
+
+            cout<<"\n"<<endl;
+            cout<<"--------------3UZD--------------"<<endl;
+            cout<<"\n"<<endl;
+
+    int iv;
+        cout<<"Iveskite egzamino ivertinimo pazymi: "<<endl;
+        cin>>iv;
+
+
+    if (iv==10) {
+        cout<<"Puiku!"<<endl;
+    } else if (iv>=9) {
+        cout<<"Labai gerai!"<<endl;
+    } else if (iv>=7) {
+        cout<<"Gerai!"<<endl;
+    } else if (iv>=5) {
+        cout<<"Patenkinamai"<<endl;
+    } else if (5>iv) {
+        cout<<"Egzaminas neslaikytas"<<endl;
+    }
+
+
+
+        cout<<"\n"<<endl;
+        cout<<"--------------4UZD--------------"<<endl;
+        cout<<"\n"<<endl;
+
+    int iv;
+        cout<<"Iveskite egzamino ivertinimo pazymi: "<<endl;
+        cin>>iv;
+
+     switch (iv) {
+         case 10:
+             cout<<"Puiku!"<<endl;
+         break;
+         case 9:
+             cout<<"Labai gerai!"<<endl;
+         break;
+         case 8: case 7:
+             cout<<"Gerai!"<<endl;
+         break;
+         case 6: case 5:
+             cout<<"Patenkinamai!"<<endl;
+         break;
+         default:
+             cout<<"Egzaminas neislaikytas!"<<endl;
+         break;
+     }
+
+
+
+    cout<<"\n"<<endl;
+    cout<<"--------------5UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    int sum = 0;
+        for (int i=1; 20>= i; i++)
+        if (i%2 !=0)
+            sum = sum + i;
+
+   { cout<<"Visu nelyginiu skaiciu suma intervale nuo 1 iki 20 lygu: "<<sum<<endl;
+    }
+
+
+    cout<<"\n"<<endl;
+    cout<<"--------------6UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    int in = 1000;
+    int sum = 0;
+
+    for (int a = 1; a<=10; a++)
+            sum = (in*5)/100 * a;
+         {   cout<<"Po 10 metu klientas gales gauti: "<<in+sum<<endl;
+        }
+
+*/
+
+    cout<<"\n"<<endl;
+    cout<<"--------------7UZD--------------"<<endl;
+    cout<<"\n"<<endl;
+
+    cout<<"Skaiciai, kurie dalinasi is 4, intervale nuo 1 iki 20"<<endl;
+    
+    int a;
+    for (int a=1; 20>=a; a++)
+        if (a%4 ==0)
+
+   {
+            cout<<a<<endl;
+}
+    return 0;
+
+
+}
